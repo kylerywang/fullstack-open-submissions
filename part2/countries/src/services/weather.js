2991d4a5235a4962fe7a303a655ae25dac0c1149
kylerywang <kyler.y.wang@gmail.com>
2023-08-10 15:48:53 -0700

    fix security issue

--- part2/countries/src/services/weather.js
+++ part2/countries/src/services/weather.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-// const api_key='3f858fbe525ea1a538624ed830dd6814'
 const api_key = process.env.REACT_APP_WEATHER_API_KEY
 const baseUrl=`https://api.openweathermap.org/data/2.5/weather?q=`
 const unit='metric'
